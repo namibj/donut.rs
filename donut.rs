01744e5a16d472c2a40c3b0ead49e8dd85a3be44
namibj <github-commit@d10.pw>
2020-08-30 17:30:08 +0000

    Forbit single-line if-else for line-based debugging.

--- donut.rs
+++ donut.rs
@@ -4,10 +4,10 @@ fn main() {
     let mut bee: f64 = 0.0;
     let mut i: f64 = 0.0;
     let mut j: f64 = 0.0;
-    
+
 
     print!("\x1b[2J");
-    
+
     loop {
         let mut z = [0.00; 1760].to_vec();
         let mut b = [' '; 1760].to_vec();
@@ -33,7 +33,7 @@ fn main() {
                 let x = 40.00 + 30.00 * dee * (l * h * m - t * n as f64);
                 let y = 12.00 + 15.00 * dee * (l * h * (n as f64) + t * m);
                 let o = (x + 80.00 * y) as usize;
-                let en = 8.00 * ( (f * e - c * d * g) * m - c * d * e - f * g - l * d * n as f64);
+                let en = 8.00 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n as f64);
 
                 if 22.00 > y && y > 0.00 && x > 0.00 && 80.00 > x && dee > z[o] {
                     z[o] = dee;
@@ -46,7 +46,14 @@ fn main() {
 
         while 1759 > k {
             k += 1;
-            print!("{}", if k % 80 == 0 { b[k] } else {'\n'})
+            print!(
+                "{}",
+                if k % 80 == 0 {
+                    b[k]
+                } else {
+                    '\n'
+                }
+            )
         }
 
         ay += 0.04;
